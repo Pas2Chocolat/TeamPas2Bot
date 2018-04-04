fa382b206345b79eda508fd24876e40893c79ccf
Pas2Chocolat <38064873+Pas2Chocolat@users.noreply.github.com>
2018-04-04 13:35:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,8 +3,9 @@ const Discord = require ("discord.js");
 var bot = Discord.Client();
 
 bot.on("ready" , function() {
-   bot.user.SetGame("TeamPas2, p!help");
-   console.log("Le bot a bien été connecté");
+    bot.user.SetGame("TeamPas2, p!help");
+    console.log("Le bot a bien été connecté");
 });
+
 bot.login("NDMwNjg2NjUxNzk1MTExOTM2.DaT0Tg.3lgB5XVdR3EmCTYGfLrmzVTJDPA");
 
